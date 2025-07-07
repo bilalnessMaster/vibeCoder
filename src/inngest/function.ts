000791e2e27f52dc25f5f756fc6bd1479c2f8573
bilalnessmaster <jijahgiga@gmail.com>
2025-07-07 16:44:58 +0000

    06 : e2b sandbpox

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,15 +1,19 @@
-import { Agent, openai, createAgent, gemini, grok } from "@inngest/agent-kit";
+import { createAgent, gemini } from "@inngest/agent-kit";
 import { inngest } from "./client";
-
-
+import { Sandbox } from '@e2b/code-interpreter';
+import { getSandboxUrl } from "./utils";
 export const helloworld = inngest.createFunction(
 
   { id: "functionName" },
 
   { event: "test/hello" },
 
-  async ({ event }) => {
+  async ({ event, step }) => {
 
+    const SandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("nte")
+      return sandbox.sandboxId;
+    })
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next,js developer . You write readable, maintainable code, You write simple Next.js & react.js snippets",
@@ -18,10 +22,14 @@ export const helloworld = inngest.createFunction(
 
     const { output } = await codeAgent.run("summerizer this text " + event.data.input);
 
-
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandboxUrl(SandboxId)
+      const host = sandbox.getHost(3000);
+      return `https://${host}`
+    })
     console.log("the outout", output)
 
-    return { output }
+    return { output, sandboxUrl }
   }
 
 )
